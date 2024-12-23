063ac03ac5172de3253ef93b5983fe0d1b655e9b
JOSUE DE SOUZA MORAIS <josue28jrd@hotmail.com>
2024-12-23 12:59:46 -0300

    Atualizar o script.js

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ function generateKeyframes() {
     const nColors = 80;
     const colors = [];
 
-    // Gerar 250 cores interpolando de rgb(255, 0, 0) para rgb(255, 165, 0)
+    // Gerar cores interpolando de rgb(255, 0, 0) para rgb(255, 165, 0)
     for (let i = 0; i < nColors; i++) {
         let r = 255;
         let g = Math.round(i * 165 / (nColors - 1));
